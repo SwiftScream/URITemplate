9b8d0bea9dd9e1db6b3ad56fccd4cb8d32663089
Alex Deem <alexdeem@gmail.com>
2023-12-20 23:28:32 +1100

    add dependency on swift-docc-plugin

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
             targets: ["ScreamURITemplate"]),
     ],
     dependencies: [
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
     ],
     targets: [
         .target(
