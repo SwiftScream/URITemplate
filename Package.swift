9f5baa06b412440fe28534b5ff0bf3bb4728b7fe
Alex Deem <alexdeem@gmail.com>
2024-11-30 09:42:16 +1100

    Update to swift 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 
 import PackageDescription
 
@@ -37,4 +37,4 @@ let package = Package(
             name: "ScreamURITemplateExample",
             dependencies: ["ScreamURITemplate"]),
     ],
-    swiftLanguageVersions: [.v5])
+    swiftLanguageModes: [.v6])
