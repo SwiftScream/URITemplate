96ce04a829fae0bcf7707548270a939473b5392a
Alex Deem <alexdeem@gmail.com>
2026-06-11 23:34:12 +1000

    fix: conform literal characters strictly to RFC

--- Sources/ScreamURITemplate/Internal/Scanner.swift
+++ Sources/ScreamURITemplate/Internal/Scanner.swift
@@ -190,7 +190,7 @@ struct Scanner {
                 }
                 endIndex = utf8.index(after: endIndex)
             } else {
-                guard literalCharacterSet.contains(unicodeScalars[endIndex]) else {
+                guard unicodeScalars[endIndex].isAllowedInLiteral() else {
                     break
                 }
                 endIndex = unicodeScalars.index(after: endIndex)
@@ -307,5 +307,35 @@ private extension UTF8.CodeUnit {
             self != .openBrace &&
             self != .pipe &&
             self != .closeBrace
+    }
+}
+
+private extension Unicode.Scalar {
+    func isAllowedInLiteral() -> Bool {
+        switch value {
+        case 0xA0...0xD7FF,
+             0xF900...0xFDCF,
+             0xFDF0...0xFFEF,
+             0x10000...0x1FFFD,
+             0x20000...0x2FFFD,
+             0x30000...0x3FFFD,
+             0x40000...0x4FFFD,
+             0x50000...0x5FFFD,
+             0x60000...0x6FFFD,
+             0x70000...0x7FFFD,
+             0x80000...0x8FFFD,
+             0x90000...0x9FFFD,
+             0xA0000...0xAFFFD,
+             0xB0000...0xBFFFD,
+             0xC0000...0xCFFFD,
+             0xD0000...0xDFFFD,
+             0xE1000...0xEFFFD,
+             0xE000...0xF8FF,
+             0xF0000...0xFFFFD,
+             0x100000...0x10FFFD:
+            true
+        default:
+            false
+        }
     }
 }
